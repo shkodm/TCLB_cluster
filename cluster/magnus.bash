7d9f2917e9a98d9ccb91aa8c402fdc74c6b3a0e8
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2018-01-30 00:14:30 +0800

    Adding magnus specific things

--- cluster/magnus.bash
+++ cluster/magnus.bash
@@ -1,6 +1,28 @@
 # magnus-.*
 
-echo "Magnus!"
+echo "Running on Magnus."
+
+RUN_COMMAND="srun"
+RUN_COMMAND_ASK="no"
+DEBUGQ_DEF="debugq"
+DEBUGQ_ASK="no"
+MAINQ_DEF="workq"
+MAINQ_ASK="no"
+RUN_GPU_DEF="n"
+RUN_GPU_ASK="no"
+MODULES_RUN_DEF="PrgEnv-cray"
+MODULES_UNLOAD_DEF="PrgEnv-cray"
+MODULES_UNLOAD_ASK="no"
+CONFOPT_DEF="--disable-cuda"
+MAX_UNITS_PER_NODE_DEF=24
+CORES_PER_UNIT_DEF=1
+CORES_PER_UNIT_FULL_DEF=1
+MAX_UNITS_PER_NODE_ASK="no"
+CORES_PER_UNIT_ASK="no"
+CORES_PER_UNIT_FULL_ASK="no"
+MODULES_ADD_DEF=""
+MODULES_ADD_ASK="no"
+MODULES_RUN_ASK="no"
 
 [ -z "$RHOME" ] && RHOME=$(echo /pawsey/cle*/apps/PrgEnv-gnu/*/gcc/*/haswell/r/*/bin/R | sed 's|bin/R||' | head -n1)
-MODULE_RUN_DEF=$(module list 2>&1 | grep PrgEnv | sed 's/^.*) //') #Getting the PrgEnv module loaded now.
+RHOME_ASK=""
