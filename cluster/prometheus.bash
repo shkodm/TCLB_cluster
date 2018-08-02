b36fb5fa5aa7cd5030c29a5e43d47ccabf5f28d1
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2018-08-02 16:24:00 +0200

    moved to new method of defining modules to load/unload

--- cluster/prometheus.bash
+++ cluster/prometheus.bash
@@ -7,7 +7,7 @@ DEBUGQ_DEF="plgrid-testing"
 DEBUGQ_ASK="no"
 RUN_COMMAND_DEF="mpirun"
 RUN_COMMAND_ASK="no"
-MODULES_UNLOAD_ASK="no"
+MODULES_BASE_ASK="no"
 
 function RUN_GPU_CHECK {
 	case "$RUN_GPU" in
