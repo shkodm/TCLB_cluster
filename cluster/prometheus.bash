52042747af46a21fbea7712ba2774d6e2cbc262f
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2018-10-08 13:45:59 +0200

    Fixing prometheus settings according to HD-7710 helpdesk issue

--- cluster/prometheus.bash
+++ cluster/prometheus.bash
@@ -13,7 +13,7 @@ function RUN_GPU_CHECK {
 	case "$RUN_GPU" in
 	y)
 		MAINQ_DEF="plgrid-gpu"
-		MODULES_RUN_DEF="plgrid/apps/cuda/9.0 plgrid/tools/openmpi/2.1.1-gcc-6.4.0"
+		MODULES_RUN_DEF="plgrid/tools/openmpi/3.0.0-gcc-4.9.2 plgrid/apps/cuda/9.0"
 		CONFOPT_DEF="--with-cuda-arch=sm_30"
 		MAX_UNITS_PER_NODE_DEF=2
 		CORES_PER_UNIT_DEF=1
