89d1366fb06cf12316845bf79e07c86e00b083ab
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2019-06-14 14:31:06 +0200

    Adding config for hyperion

---
+++ cluster/hyperion.bash
@@ -0,0 +1,49 @@
+# hyp-.*
+
+echo "You are on the Hyperion cluster"
+
+CONFOPT_DEF=""
+DEBUGQ_DEF="cpus"
+DEBUGQ_ASK="no"
+RUN_COMMAND_DEF="mpirun"
+RUN_COMMAND_ASK="no"
+MODULES_BASE_ASK="no"
+
+function RUN_GPU_CHECK {
+	case "$RUN_GPU" in
+	y)
+		MAINQ_DEF="gpus"
+		MODULES_RUN_DEF="hyp/CUDA/10.1 hyp/mpi/openmpi/4.0.1"
+		CONFOPT_DEF="--with-cuda-arch=sm_30"
+		MAX_UNITS_PER_NODE_DEF=2
+		CORES_PER_UNIT_DEF=1
+		CORES_PER_UNIT_FULL_DEF=1
+		MAX_UNITS_PER_NODE_ASK="no"
+		CORES_PER_UNIT_ASK="no"
+		CORES_PER_UNIT_FULL_ASK="no"
+		;;
+	n)
+		MAINQ_DEF="cpus"
+		MODULES_RUN_DEF="hyp/mpi/openmpi/4.0."
+		CONFOPT_DEF="--disable-cuda"
+		MAX_UNITS_PER_NODE_DEF=32
+		CORES_PER_UNIT_DEF=1
+		CORES_PER_UNIT_FULL_DEF=1
+		MAX_UNITS_PER_NODE_ASK="no"
+		CORES_PER_UNIT_ASK="no"
+		CORES_PER_UNIT_FULL_ASK="no"
+		;;
+	*)
+		echo "RUN_GPU should be y or n!"
+		return 1;
+	esac
+	return 0
+}
+MAINQ_ASK="no"
+
+MODULES_ADD_DEF=""
+MODULES_ADD_ASK="no"
+MODULES_RUN_ASK="no"
+
+GRANT_DEF=""
+GRANT_ASK="no"
