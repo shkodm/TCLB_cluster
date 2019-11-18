b0b1988504299f471a9bf7cc8c44073322ef9890
ggruszczynski <ggruszczynski@gmail.com>
2019-11-18 16:40:38 +0100

    removing CORES_PER_UNIT_DEF

--- cluster/magnus.bash
+++ cluster/magnus.bash
@@ -10,7 +10,6 @@ def MODULES_RUN "PrgEnv-gnu"
 fix MODULES_BASE "-PrgEnv-cray"
 def CONFOPT "--disable-cuda --with-cpp-flags='-O2' CXX=CC CC=cc"
 fix MAX_TASKS_PER_NODE 24
-fix CORES_PER_TASK 1
 fix CORES_PER_TASK_FULL 1
 fix MODULES_ADD ""
 fix PREPARE "ulimit -S -s 81920"
